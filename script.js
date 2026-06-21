8916e3837f0d67f400dd7f23255f6f57b5ec6f33
Ouarich Inas <inasouarich@gmail.com>
2026-06-21 23:00:24 +0200

    liste des pistes

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ function initSpotifyApp() {
         selectedTrack: null,
         isModalOpen: false,
         searchQuery: '',
+        init() {
+            fetch('./data.json')
+                .then(response => {
+                    if (!response.ok) throw new Error("Impossible de lire data.json");
+                    return response.json();
+                })
+                .then(data => {
+                    this.tracks = data;
+                })
+                .catch(error => console.error("Erreur au chargement JSON :", error));
+        }
     };
 }
 if (window.Alpine) {
